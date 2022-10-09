024a18752afe6538831e48e23f6131bcfd8c77f5
Zeljko <zeljko.cavic@playstudios.com>
2022-10-08 17:00:44 -0700

    Show a list of frameworks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const stories = [
+        {
+            title: "React",
+            url: "some url",
+            author: "John Doe",
+            key: 1
+        },
+        {
+            title: "Vue",
+            url: "some url 2",
+            author: "John Doe 2",
+            key: 2
+        },
+        {
+            title: "Angular",
+            url: "some url 2",
+            author: "John Doe 2",
+            key: 3
+        },
+        {
+            title: "NativeJavascript",
+            url: "some url 2",
+            author: "John Doe 2",
+            key: 4
+        }
+    ];
+
+    const List = (props) => (
+        <ul>
+            {props.list.map((item) => (
+                <Item key={item.key} item={item}></Item>
+            ))}
+        </ul>
+    );
+
+    const Item = (props) => (
+        <li>
+            <span>
+                <a href={props.item.url}>{props.item.title}</a>
+            </span>
+        </li>
+    );
+
+    const Search = () => {
+        const [searchTerm, setSearchTerm] = React.useState("");
+        const handleChange = (event) => {
+            setSearchTerm(event.target.value);
+        };
+        return (
+            <div>
+                <label htmlFor="search">Search:</label>
+                <input id="search" type="text" onChange={handleChange}></input>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <List list={stories}></List>
+        </div>
+    );
 }
 
 export default App;
